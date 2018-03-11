749bf0cd59c251ee49a1114452ecb9c904b4f829
bri <briegrant416@gmail.com>
2018-03-11 21:29:02 +0300

    Add details test.py

--- inst/tests.py
+++ inst/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Comment
 
-# Create your tests here.
+
+
+class CommentTestClass(TestCase):
+
+    def setUp(self):
+        
+        self.new_comment = Comment(comment= "comment")
+        self.new_comment.save()
+
+  
